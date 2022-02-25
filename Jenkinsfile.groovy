5438f9596b22bc5b44b6d3feee2059523a64cb92
Ugonna Ekoro <uekoroxx@oceusnetworks.com>
2022-02-25 14:19:54 -0600

    jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,32 @@
+pipeline{
+    agent any
+    stages{
+        stage('SCM'){
+            steps{
+                echo 'SCM Checkout'
+                git branch: 'main', url: 'https://github.com/annogugonna/ONCAST.git'
+            }
+        }
+        stage('Fetch Build Repository'){
+            steps{
+                git branch: 'main', url: 'https://github.com/annogugonna/DEV.git'
+            }
+        }
+        stage('Run Playbook'){
+            steps{
+                ansiblePlaybook credentialsId: 'keys', disableHostKeyChecking: true, installation: 'Ansible', inventory: 'inventory.txt', playbook: 'Build.yml', tags: 'xmc'
+            }
+        }
+        stage('Tar image'){
+            steps{
+                sh "docker save hugoboss192/hariapp:dockertag --output cmx-image.tar"
+            }
+        }
+        stage('Archive Artifacts'){
+            steps {
+                archiveArtifacts artifacts: 'cmx-image.tar', onlyIfSuccessful: true
+                cleanWs cleanWhenAborted: false, cleanWhenFailure: false, cleanWhenNotBuilt: false, cleanWhenUnstable: false
+            }
+        }
+    }
+}       
